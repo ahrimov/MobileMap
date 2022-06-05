e80769e0e134251c3fc127142171b8b81a4fb5de
ahrimov <ahrimovanton@mail.ru>
2022-06-06 02:03:48 +0300

    Add interaction with local bd

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,38 @@
+//import { copyDatabaseFile } from "./openDB.js"
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 var all_features = []
 
 function onDeviceReady() {
   console.log('ready')
+
+  copyDatabaseFile("sample.db").then(function () {
+    var db = sqlitePlugin.openDatabase("sample.db");
+    db.readTransaction(function (txn){
+      txn.executeSql('SELECT data FROM idx_свеча_GEOMETRY_node', [], function(tx, res){
+        console.log('Successfully read from pre-populated DB:')
+        console.log(JSON.stringify(res.rows.item(1)))
+        //var data = res.rows.item(1).data
+        /*var data = new Uint8Array([
+          137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,8,0,0,
+          0,8,8,2,0,0,0,75,109,41,220,0,0,0,34,73,68,65,84,8,215,99,120,
+          173,168,135,21,49,0,241,255,15,90,104,8,33,129,83,7,97,163,136,
+          214,129,93,2,43,2,0,181,31,90,179,225,252,176,37,0,0,0,0,73,69,
+          78,68,174,66,96,130]);*/
+        let blob = new Blob([data], {type: 'image/png'})
+        var url = URL.createObjectURL(blob)
+        var img = new Image()
+        img.src = url
+        console.log("data length: " + data.length);
+        console.log("url: " + url);
+        document.body.appendChild(img);
+        //console.log(JSON.stringify(res.rows.item(0)))
+      })
+    })
+  })
+
+
 
   window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory + "json.json", gotFile, fail);
   var raster = new ol.layer.Tile({
@@ -49,7 +78,7 @@ const vector = new ol.layer.Vector({
   function createVectorSourceFromJson(filename){
     const json = JSON.parse(filename) 
     for (let line of json){
-      geojson = JSON.parse(line['st_asgeojson'])
+      var geojson = JSON.parse(line['st_asgeojson'])
       var point = new ol.geom.Point(geojson['coordinates'].reverse())
       point.transform('EPSG:4326', 'EPSG:3857');
       var featurePoint = new ol.Feature({
@@ -95,7 +124,7 @@ const vector = new ol.layer.Vector({
     }
     else if(features.length > 0 ){
       var line = ''
-      for (feature of features){
+      for (let feature of features){
         line += `<tr onclick="moduleFeatureClick(`+ feature.get('id') +`)"><td>` + feature.get('name') + `</td></tr>`
         $('#manyFeature .table').html(line)
       }
